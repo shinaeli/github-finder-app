e99545ad559efe1637fe7981a6b54db32bc22842
shinaeli <omotoshoelisha@gmail.com>
2023-10-28 14:12:06 +0100

    my first commit

--- src/context/alert/AlertContext.js
+++ src/context/alert/AlertContext.js
@@ -1,24 +1,28 @@
-import {createContext, useReducer} from 'react'
+import { createContext, useReducer } from 'react'
 import alertReducer from './AlertReducer'
 
 const AlertContext = createContext()
 
-export const AlertProvider = ({children}) => {
-    const initialState = null
+export const AlertProvider = ({ children }) => {
+  const initialState = null
 
-    const [state, dispatch] = useReducer(alertReducer, initialState)
+  const [state, dispatch] = useReducer(alertReducer, initialState)
 
-    // Set an alert
-    const setAlert = (msg, type) => {
-        dispatch({
-            type: 'SET_ALERT',
-            payload: {msg, type},
-        })
+  // Set an alert
+  const setAlert = (msg, type) => {
+    dispatch({
+      type: 'SET_ALERT',
+      payload: { msg, type },
+    })
 
-        setTimeout(() => dispatch({ type: 'REMOVE_ALERT' }), 3000)
-    }
+    setTimeout(() => dispatch({ type: 'REMOVE_ALERT' }), 3000)
+  }
 
-    return <AlertContext.Provider value={{alert: state, setAlert}}>{children}</AlertContext.Provider>
+  return (
+    <AlertContext.Provider value={{ alert: state, setAlert }}>
+      {children}
+    </AlertContext.Provider>
+  )
 }
 
 export default AlertContext
